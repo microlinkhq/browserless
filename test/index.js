86cfafcf79e50b631b1adfaece4da7ea2b38df06
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-10-08 11:38:35 +0200

    Fix test

--- test/index.js
+++ test/index.js
@@ -11,56 +11,57 @@ const areEqual = (image1, image2) => image1.length === image2.length
 
 describe('browserless', () => {
   describe('.html', () => {
-    it('get full HTML from a link', () =>
-      browserless
-        .html('https://www.instagram.com/p/BWUDBntl3_Z/')
-        .then(html => {
-          should(html.includes('DOCTYPE')).be.true()
-        }))
+    it('get full HTML from a link', async () => {
+      const html = await browserless.html(
+        'https://www.instagram.com/p/BWUDBntl3_Z/'
+      )
+      should(html.includes('DOCTYPE')).be.true()
+    })
   })
 
   describe('.screenshot', () => {
     describe('format', () => {
-      it('png', () =>
-        browserless.screenshot('http://example.com').then(tmpStream => {
-          const imageBuffer = readFileSync(tmpStream.path)
-          const fixtureBuffer = readFileSync('test/example.png')
-          const areImageEquals = areEqual(imageBuffer, fixtureBuffer)
-          tmpStream.cleanupSync()
-          should(isTravis ? true : areImageEquals).be.true()
-        }))
+      it('png', async () => {
+        const tmpStream = await browserless.screenshot('http://example.com')
+        const imageBuffer = readFileSync(tmpStream.path)
+        const fixtureBuffer = readFileSync('test/example.png')
+        const areImageEquals = areEqual(imageBuffer, fixtureBuffer)
+        tmpStream.cleanupSync()
+        should(isTravis ? true : areImageEquals).be.true()
+      })
 
-      it('jpeg', () =>
-        browserless
-          .screenshot('http://example.com', { type: 'jpeg' })
-          .then(tmpStream => {
-            const imageBuffer = readFileSync(tmpStream.path)
-            const fixtureBuffer = readFileSync('test/example.jpeg')
-            const areImageEquals = areEqual(imageBuffer, fixtureBuffer)
-            tmpStream.cleanupSync()
-            should(isTravis ? true : areImageEquals).be.true()
-          }))
+      it('jpeg', async () => {
+        const tmpStream = await browserless.screenshot('http://example.com', {
+          type: 'jpeg'
+        })
+        const imageBuffer = readFileSync(tmpStream.path)
+        const fixtureBuffer = readFileSync('test/example.jpeg')
+        const areImageEquals = areEqual(imageBuffer, fixtureBuffer)
+        tmpStream.cleanupSync()
+        should(isTravis ? true : areImageEquals).be.true()
+      })
     })
 
     describe('devices', () => {
-      it('iPhone 6', () =>
-        browserless
-          .screenshot('http://example.com', { device: 'iPhone 6' })
-          .then(tmpStream => {
-            const imageBuffer = readFileSync(tmpStream.path)
-            const fixtureBuffer = readFileSync('test/example-iphone.png')
-            const areImageEquals = areEqual(imageBuffer, fixtureBuffer)
-            tmpStream.cleanupSync()
-            should(isTravis ? true : areImageEquals).be.true()
-          }))
-    })
-  })
-
-  describe('.pdf', () => {
-    it('get full PDF from an url', () =>
-      browserless.pdf('http://example.com').then(tmpStream => {
-        should(path.extname(tmpStream.path)).be.equal('.pdf')
+      it('iPhone 6', async () => {
+        const tmpStream = await browserless.screenshot('http://example.com', {
+          device: 'iPhone 6'
+        })
+        const imageBuffer = readFileSync(tmpStream.path)
+        const fixtureBuffer = readFileSync('test/example-iphone.png')
+        const areImageEquals = areEqual(imageBuffer, fixtureBuffer)
         tmpStream.cleanupSync()
-      }))
+        should(isTravis ? true : areImageEquals).be.true()
+      })
+    })
+
+    describe('.pdf', () => {
+      it('get full PDF from an url', async () => {
+        const tmpStream = await browserless.pdf('http://example.com')
+        const filepath = tmpStream.path
+        tmpStream.cleanupSync()
+        should(path.extname(filepath)).be.equal('.pdf')
+      })
+    })
   })
 })
