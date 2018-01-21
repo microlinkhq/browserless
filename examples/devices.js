ee20b13e4d5489d9e41f70bc88c9d256a58c2b29
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-01-21 11:03:04 +0100

    Add jsx output format

--- examples/devices.js
+++ examples/devices.js
@@ -3,15 +3,19 @@
 const { devices } = require('..')
 const { chain } = require('lodash')
 
-console.log(`| name | width | height | scale | mobile? | touch? | landscape? |`)
-console.log(
-  `|------|-------|--------|-------------------|----------|----------|-------------|`
-)
+const HEADER = {
+  markdown: () => {
+    console.log(
+      `| name | width | height | scale | mobile? | touch? | landscape? |`
+    )
+    console.log(
+      `|------|-------|--------|-------------------|----------|----------|-------------|`
+    )
+  }
+}
 
-const deviceStrings = chain(devices)
-  .orderBy(['name'], ['asc'])
-  .map(device => {
-    const { name, viewport } = device
+const ROW = {
+  markdown: (name, viewport) => {
     const {
       width,
       height,
@@ -22,7 +26,35 @@ const deviceStrings = chain(devices)
     } = viewport
 
     return `| \`${name}\` | ${width} | ${height} | ${deviceScaleFactor} | ${isMobile} | ${hasTouch} | ${isLandscape} |`
-  })
+  },
+  jsx: (name, viewport) => {
+    const {
+      width,
+      height,
+      deviceScaleFactor,
+      isMobile,
+      hasTouch,
+      isLandscape
+    } = viewport
+
+    return `<Row>
+  <BoldCell>${name.toLowerCase()}</BoldCell>
+  <Cell>${width}</Cell>
+  <Cell>${height}</Cell>
+  <Cell>${deviceScaleFactor}</Cell>
+  <Cell>${isMobile}</Cell>
+  <Cell>${hasTouch}</Cell>
+  <Cell>${isLandscape}</Cell>
+</Row>`
+  }
+}
+
+const format = process.argv[2] || 'markdown'
+
+const deviceStrings = chain(devices)
+  .orderBy(['name'], ['asc'])
+  .map(({ name, viewport }) => ROW[format](name, viewport))
   .value()
 
+HEADER[format] && HEADER[format]()
 deviceStrings.forEach(str => console.log(str))
