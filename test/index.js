07c1867ab3a065e15c6ab2449efb8d0b8aecb5b3
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-03 14:39:10 +0100

    WIP

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 'use strict'
 
-const { readFileSync } = require('fs')
-const isTravis = require('is-travis')
+const { createReadStream } = require('fs')
+const pixelmatch = require('pixelmatch')
+const { promisify } = require('util')
+const { PNG } = require('pngjs')
 const should = require('should')
 const path = require('path')
 
@@ -9,7 +11,7 @@ const browserless = require('..')({
   args: ['--disable-gpu', '--single-process', '--no-zygote', '--no-sandbox']
 })
 
-const isBufferEqual = (buff1, buff2) => buff1.length === buff2.length
+// const isBufferEqual = (buff1, buff2) => buff1.length === buff2.length
 
 describe('browserless', () => {
   describe('.html', () => {
@@ -19,28 +21,67 @@ describe('browserless', () => {
     })
   })
 
+  const getDiffPixels = promisify(({ imageOnePath, imageTwoPath }, cb) => {
+    let filesRead = 0
+
+    const imageOne = createReadStream(imageOnePath)
+      .pipe(new PNG())
+      .on('parsed', doneReading)
+
+    const imageTwo = createReadStream(imageTwoPath)
+      .pipe(new PNG())
+      .on('parsed', doneReading)
+
+    function doneReading () {
+      if (++filesRead < 2) return
+
+      if (imageOne.width !== imageTwo.width) {
+        return cb(new Error('width are different'))
+      }
+      if (imageOne.height !== imageTwo.height) {
+        return cb(new Error('height are different'))
+      }
+
+      const diff = new PNG({ width: imageOne.width, height: imageOne.height })
+
+      return cb(
+        null,
+        pixelmatch(
+          imageOne.data,
+          imageTwo.data,
+          diff.data,
+          imageOne.width,
+          imageOne.height,
+          { threshold: 0.1 }
+        )
+      )
+    }
+  })
+
   describe('.screenshot', () => {
     describe('format', () => {
       it('png', async () => {
         const tmpStream = await browserless.screenshot('http://example.com')
 
-        const image = readFileSync(tmpStream.path)
-        const fixture = readFileSync('test/example.png')
+        const numDiffPixels = await getDiffPixels({
+          imageOnePath: tmpStream.path,
+          imageTwoPath: 'test/example.png'
+        })
 
         tmpStream.cleanupSync()
-        if (!isTravis) should(isBufferEqual(image, fixture)).be.true()
+        should(numDiffPixels).be.equal(0)
       })
 
-      it('jpeg', async () => {
+      xit('jpeg', async () => {
         const tmpStream = await browserless.screenshot('http://example.com', {
           type: 'jpeg'
         })
 
-        const image = readFileSync(tmpStream.path)
-        const fixture = readFileSync('test/example.jpeg')
+        // const image = readFileSync(tmpStream.path)
+        // const fixture = readFileSync('test/example.jpeg')
 
         tmpStream.cleanupSync()
-        if (!isTravis) should(isBufferEqual(image, fixture)).be.true()
+        // if (!isTravis) should(isBufferEqual(image, fixture)).be.true()
       })
     })
 
@@ -50,25 +91,27 @@ describe('browserless', () => {
           device: 'iPhone 6'
         })
 
-        const image = readFileSync(tmpStream.path)
-        const fixture = readFileSync('test/example-iphone.png')
+        const numDiffPixels = await getDiffPixels({
+          imageOnePath: tmpStream.path,
+          imageTwoPath: 'test/example-iphone.png'
+        })
 
         tmpStream.cleanupSync()
-        if (!isTravis) should(isBufferEqual(image, fixture)).be.true()
+        should(numDiffPixels).be.equal(0)
       })
     })
 
     describe('.pdf', () => {
-      it('get full PDF from an url', async () => {
+      xit('get full PDF from an url', async () => {
         const tmpStream = await browserless.pdf('http://example.com')
 
-        const pdf = readFileSync(tmpStream.path)
-        const fixture = readFileSync('test/example.pdf')
+        // const pdf = readFileSync(tmpStream.path)
+        // const fixture = readFileSync('test/example.pdf')
 
         tmpStream.cleanupSync()
 
         should(path.extname(tmpStream.path)).be.equal('.pdf')
-        if (!isTravis) should(isBufferEqual(pdf, fixture)).be.true()
+        // if (!isTravis) should(isBufferEqual(pdf, fixture)).be.true()
       })
     })
   })
