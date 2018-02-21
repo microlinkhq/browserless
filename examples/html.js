a41a88b3cb238ae939a1ae131bd82b8fb71793c4
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-21 23:33:59 +0100

    Update html.js

--- examples/html.js
+++ examples/html.js
@@ -7,6 +7,7 @@ const browserless = createBrowserless()
 const stringStream = require('string-to-stream')
 
 const url = new URL(process.argv[2])
+
 ;(async () => {
   const html = await browserless.html(url.toString())
   const stream = stringStream(html)
