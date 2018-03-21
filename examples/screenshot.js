9b22e996ac90203705986635e9efd91d8589a9b9
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-03-21 10:38:50 +0100

    Print screenshot on terminal when is possible

--- examples/screenshot.js
+++ examples/screenshot.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { URL } = require('url')
+const termImg = require('term-img')
 
 const createBrowserless = require('..')
 const browserless = createBrowserless()
@@ -9,12 +10,16 @@ const url = new URL(process.argv[2])
 ;(async () => {
   const tmpStream = await browserless.screenshot(url.toString(), {
     tmpOpts: {
-      path: './',
       name: `${url.hostname}.${Date.now()}`
     }
   })
 
-  const filepath = tmpStream.path
-  console.log(filepath)
-  process.exit()
+  try {
+    termImg(tmpStream.path)
+    await tmpStream.cleanup()
+    process.exit()
+  } catch (err) {
+    console.log(`Screenshot saved at '${tmpStream.path}'`)
+    process.exit()
+  }
 })()
