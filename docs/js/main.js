ef5ede895c31108dbd41439aea6f6c0a0a6d8383
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-04-06 00:45:56 +0200

    Add codecopy

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,17 @@
+/* global codecopy */
+
 window.$docsify = {
   name: 'browserless',
   repo: 'kikobeats/browserless',
   maxLevel: 3,
   executeScript: true,
   auto2top: true,
-  noEmoji: true
+  noEmoji: true,
+  plugins: [
+    function (hook, vm) {
+      hook.ready(function () {
+        codecopy('pre')
+      })
+    }
+  ]
 }
