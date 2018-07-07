cd01835394ee099411b75427a52e7f4eb8ae7bf7
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-07-07 14:35:54 +0200

    Improve example

--- examples/evaluate.js
+++ examples/evaluate.js
@@ -6,16 +6,26 @@ const createBrowserless = require('..')
 const browserless = createBrowserless()
 const stringStream = require('string-to-stream')
 
-const getUrlInfo = browserless.evaluate((page, response) => ({
-  statusCode: response.status(),
-  url: response.url(),
-  redirectUrls: response.request().redirectChain()
-}))
+const getUrlInfo = browserless.evaluate((page, response) => {
+  const redirectChain = response.request().redirectChain()
+  return {
+    statusCode: response.status(),
+    url: response.url(),
+    redirectUrls: redirectChain.map(req => req.url()),
+    redirectStatusCodes: redirectChain.map(req => req.response().status())
+  }
+})
 
 const url = new URL(process.argv[2])
 ;(async () => {
-  const info = await getUrlInfo(url.toString())
-  const stream = stringStream(JSON.stringify(info, null, 2))
-  stream.pipe(process.stdout)
-  stream.on('end', process.exit)
+  try {
+    const info = await getUrlInfo(url.toString())
+    const data = { ...info, requestUrl: url.toString() }
+    const stream = stringStream(JSON.stringify(data, null, 2))
+    stream.pipe(process.stdout)
+    stream.on('end', process.exit)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
 })()
