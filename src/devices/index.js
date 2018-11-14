7c9f5f782ad42a7b04a9b0bf05d6c4542f7b5caa
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-11-14 18:56:16 +0100

    Unify method params

--- src/devices/index.js
+++ src/devices/index.js
@@ -6,7 +6,9 @@ const customDevices = require('./devices.json')
 const devices = puppeteerDevices.concat(customDevices)
 
 const getDevice = deviceName =>
-  devices.find(device => device.name.toLowerCase() === deviceName.toLowerCase())
+  devices.find(
+    device => device.name.toLowerCase() === deviceName.toLowerCase()
+  ) || {}
 
 module.exports = devices
 module.exports.getDevice = getDevice
