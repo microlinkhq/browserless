a19bd3da9b99d4238011f303f03e4d790c7a9cee
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-03-09 18:34:31 +0100

    refactor: move pdf into a separate module

--- packages/browserless/src/index.js
+++ packages/browserless/src/index.js
@@ -83,32 +83,7 @@ module.exports = ({
     return page.screenshot({ type, ...args })
   })
 
-  const pdf = wrapError(page => async (url, opts = {}) => {
-    const {
-      format = 'A4',
-      margin = {
-        top: '0.25cm',
-        right: '0.25cm',
-        bottom: '0.25cm',
-        left: '0.25cm'
-      },
-      media = 'screen',
-      printBackground = true,
-      scale = 0.65,
-      viewport,
-      ...args
-    } = opts
-
-    await page.emulateMedia(media)
-    await goto(page, { url, ...args })
-    return page.pdf({
-      margin,
-      format,
-      printBackground,
-      scale,
-      ...args
-    })
-  })
+  const pdf = wrapError(require('@browserless/pdf'))
 
   return {
     browser,
