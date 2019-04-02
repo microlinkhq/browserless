e370770f5cc78a57aaeedfefea0b11289b980562
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-02 21:49:59 +0200

    feat: add builtin adblock

--- packages/goto/src/is-tracker.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const isTrackingDomain = require('is-tracking-domain')
-
-const WHITELIST_RESOURCE_DOMAINS = ['twimg.com', 'adobe.com']
-
-module.exports = domain => isTrackingDomain(domain, { exclude: WHITELIST_RESOURCE_DOMAINS })
