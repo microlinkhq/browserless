dc77cec1383c7913987dd47d87d6a647d43e607b
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-09-18 18:17:33 +0200

    build: support space between coookie delimiter

--- packages/goto/test/parse-cookies.js
+++ packages/goto/test/parse-cookies.js
@@ -27,3 +27,27 @@ test('parse cookies from string with `; ` delimiter', t => {
     }
   ])
 })
+
+test('parse cookies from string with `;` delimiter', t => {
+  const url = 'https://example.com'
+  const cookiesStr = 'foo=bar; hello=world'
+
+  const cookies = parseCookies(url, cookiesStr)
+
+  t.deepEqual(cookies, [
+    {
+      name: 'foo',
+      value: 'bar',
+      domain: '.example.com',
+      url: 'https://example.com',
+      path: '/'
+    },
+    {
+      name: 'hello',
+      value: 'world',
+      domain: '.example.com',
+      url: 'https://example.com',
+      path: '/'
+    }
+  ])
+})
