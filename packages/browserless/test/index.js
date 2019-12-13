5e47ba2f0bb94b0b73b79ecc6c04419d7f0e5162
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-12-14 00:48:14 +0100

    test: update snapshot

--- packages/browserless/test/index.js
+++ packages/browserless/test/index.js
@@ -6,15 +6,12 @@ const createBrowserless = require('../src')
 require('@browserless/test')(createBrowserless)
 ;['pdf', 'screenshot', 'html', 'text'].forEach(method => {
   test(`.${method} wrap errors`, async t => {
-    const browserless = createBrowserless()
     const timeout = 50
-
-    const error = await t.throwsAsync(
-      browserless[method]('https://example.com', { timeout })
-    )
+    const browserless = createBrowserless({ timeout })
+    const error = await t.throwsAsync(browserless[method]('https://example.com'))
 
     t.is(error.name, 'TimeoutError')
-    t.is(error.message, `Navigation timeout of ${timeout} ms exceeded`)
+    t.is(error.message, `Promise timed out after ${timeout} milliseconds`)
 
     const browser = await browserless.browser
     const pages = await browser.pages()
