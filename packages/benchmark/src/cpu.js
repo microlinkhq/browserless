df0eddf223a28d2b900c136c9f223fbbcf58e4f6
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-03-05 14:39:38 +0100

    fix: linter

--- packages/benchmark/src/cpu.js
+++ packages/benchmark/src/cpu.js
@@ -9,8 +9,6 @@ const ITERATIONS = [...Array(N).keys()]
 
 bench('calculate a sieve value', async function (b) {
   b.start()
-  for (const i in ITERATIONS) {
-    sieveOfErathosthenes(33554432).length === 2063689
-  }
+  ITERATIONS.forEach(() => sieveOfErathosthenes(33554432))
   b.end()
 })
