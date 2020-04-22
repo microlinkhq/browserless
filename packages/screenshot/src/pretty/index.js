f09ca56bfb1ec1d7a033fa42a91590afb1481659
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-22 16:31:11 +0200

    fix: html can start with space
    thanks @RodolfoCamposGlz for reporting

--- packages/screenshot/src/pretty/index.js
+++ packages/screenshot/src/pretty/index.js
@@ -9,6 +9,8 @@ const getHtml = require('./html')
 
 const { injectScripts, injectStyles } = require('@browserless/goto')
 
+const HTML_REGEX = /^\s*</
+
 module.exports = async (page, response, { codeScheme, contentType, styles, scripts, modules }) => {
   const isHTML = contentType === 'html'
   const [theme, payload, prism] = await Promise.all([
@@ -17,9 +19,7 @@ module.exports = async (page, response, { codeScheme, contentType, styles, scrip
     getPrism
   ])
 
-  if (isHTML && payload.startsWith('<')) {
-    return
-  }
+  if (isHTML && HTML_REGEX.test(payload)) return
 
   const html = getHtml(payload, { contentType, prism, theme })
   await page.setContent(html)
