6164b232f9bfd35287ad48855d555f83dad3cac0
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-22 21:31:07 +0200

    refactor: generate image snapshots

--- packages/test/index.js
+++ packages/test/index.js
@@ -1,60 +1,59 @@
 'use strict'
 
 const { imgDiff } = require('img-diff-js')
+const existsFile = require('exists-file')
+const { copy } = require('fs-extra')
 const temp = require('temperment')
 const pdf = require('pdf-parse')
-const isCI = require('is-ci')
 const path = require('path')
 const test = require('ava')
 
-const looksSame = async (actual, expected) => {
-  const diff = await imgDiff({
-    actualFilename: actual,
-    expectedFilename: expected
-  })
-  return diff.imagesAreSame
+const looksSame = async (actualFilename, expectedFilename) => {
+  const { imagesAreSame } = await imgDiff({ actualFilename, expectedFilename })
+  return imagesAreSame
 }
 
+const imageSnapshot = async (t, expectedFilename, filename) => {
+  const actualFilename = path.resolve(__dirname, `snapshots/${filename}`)
+  if (!(await existsFile(actualFilename))) {
+    await copy(expectedFilename, actualFilename)
+    return true
+  }
+  const isSame = await looksSame(expectedFilename, actualFilename)
+  return isSame
+}
 module.exports = createBrowserless => {
   test('.html', async t => {
     const browserless = createBrowserless()
     const html = await browserless.html('https://example.com')
-    t.true(html.includes('DOCTYPE'))
+    t.snapshot(html)
   })
-  ;(isCI ? test.skip : test)('.screenshot (png)', async t => {
+
+  test('.screenshot (png)', async t => {
     const browserless = createBrowserless()
     const filepath = temp.file({ extension: 'png' })
-    await browserless.screenshot('http://example.com', {
-      path: filepath
-    })
-
-    t.true(await looksSame(filepath, path.resolve(__dirname, 'fixtures/example.png')))
+    await browserless.screenshot('http://example.com', { path: filepath })
+    t.true(await imageSnapshot(t, filepath, 'example.png'))
   })
-  ;(isCI ? test.skip : test)('.screenshot (jpeg)', async t => {
+
+  test('.screenshot (jpeg)', async t => {
     const browserless = createBrowserless()
     const filepath = temp.file({ extension: 'jpeg' })
-    await browserless.screenshot('http://example.com', {
-      type: 'jpeg',
-      path: filepath
-    })
-
-    t.true(await looksSame(filepath, path.resolve(__dirname, 'fixtures/example.jpeg')))
+    await browserless.screenshot('http://example.com', { type: 'jpeg', path: filepath })
+    t.true(await imageSnapshot(t, filepath, 'example.jpeg'))
   })
-  ;(isCI ? test.skip : test)('devices', async t => {
+
+  test('.screenshot with device emulation', async t => {
     const browserless = createBrowserless()
     const filepath = temp.file({ extension: 'png' })
-    await browserless.screenshot('http://example.com', {
-      device: 'iPhone 6',
-      path: filepath
-    })
-
-    t.true(await looksSame(filepath, path.resolve(__dirname, 'fixtures/example-iphone.png')))
+    await browserless.screenshot('http://example.com', { device: 'iPhone 6', path: filepath })
+    t.true(await imageSnapshot(t, filepath, 'iphone.png'))
   })
-  ;(isCI ? test.skip : test)('.pdf', async t => {
+
+  test('.pdf', async t => {
     const browserless = createBrowserless()
     const buffer = await browserless.pdf('http://example.com')
     const data = await pdf(buffer)
-
     const text = data.text.trim().replace('Example\nDomain', 'Example Domain')
     t.snapshot(text)
   })
