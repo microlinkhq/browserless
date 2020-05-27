1fbf7a64b79bd1e06a85a5bbe612397237b4ead7
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-27 11:59:59 +0200

    test: run on parallel

--- packages/lighthouse/test/index.js
+++ packages/lighthouse/test/index.js
@@ -4,19 +4,19 @@ const test = require('ava')
 
 const lighthouse = require('..')
 
-test.serial('passing custom configuration', async t => {
+test('passing custom configuration', async t => {
   const url = 'https://kikobeats.com'
   const report = await lighthouse(url, { onlyAudits: ['accessibility'], device: 'mobile' })
   t.is(report.configSettings.emulatedFormFactor, 'mobile')
 })
 
-test.serial('passing a different serializer', async t => {
+test('passing a different serializer', async t => {
   const url = 'https://kikobeats.com'
   const report = await lighthouse(url, { onlyAudits: ['accessibility'], output: 'html' })
   t.true(report.startsWith('<!--'))
 })
 
-test.serial('handle timeout', async t => {
+test('handle timeout', async t => {
   const url = 'https://kikobeats.com'
 
   const error = await t.throwsAsync(
