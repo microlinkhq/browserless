af8a9f4882fe3be3201da1f6aeef62ef2856832a
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-02 16:37:31 +0200

    fix: ensure background is a string
    related: https://sentry.io/share/issue/06dd1a8ec92c45f8a5ae5e9dade9bb40/

--- packages/screenshot/src/overlay/index.js
+++ packages/screenshot/src/overlay/index.js
@@ -30,7 +30,7 @@ module.exports = async (screenshot, { browser: theme, background, path }) => {
     ? [{ input: browserOverlay }, { input: screenshot }]
     : [{ input: screenshot }]
 
-  const image = sharp(await getBackground(background)).composite(inputs)
+  const image = sharp(await getBackground(String(background))).composite(inputs)
 
   return path ? image.toFile(path) : image.toBuffer()
 }
