f0270d24edb767989a92b301a9d90dd1c61dbee3
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-09-06 15:44:31 +0200

    fix: linter

--- packages/screenshot/src/is-white-screenshot.js
+++ packages/screenshot/src/is-white-screenshot.js
@@ -5,7 +5,7 @@ const jimp = require('jimp')
 module.exports = async buffer => {
   const image = await jimp.read(buffer)
   const firstPixel = image.getPixelColor(0, 0)
-  
+
   for (let i = 0; i < image.getHeight(); i++) {
     for (let j = 0; j < image.getWidth(); j++) {
       if (firstPixel !== image.getPixelColor(j, i)) return false
