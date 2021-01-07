874dfbe18f483f6df2274e113e33ca9a574d13b2
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-01-08 00:15:17 +0100

    fix(lighthouse): ensure exception is an error

--- packages/goto/src/index.js
+++ packages/goto/src/index.js
@@ -393,6 +393,19 @@ module.exports = ({
 
     if (isWaitUntilAuto) await waitUntilAuto(page, { response: value, timeout })
 
+    await page.evaluate(async () => {
+      const selectors = Array.from(document.querySelectorAll('img'))
+      await Promise.all(
+        selectors.map(img => {
+          if (img.complete) return
+          return new Promise((resolve, reject) => {
+            img.addEventListener('load', resolve)
+            img.addEventListener('error', reject)
+          })
+        })
+      )
+    })
+
     return { response: value, device }
   }
 
