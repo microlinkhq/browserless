be40e1eea11091ec010dfd32c75ce512aa4550d7
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-03-13 20:42:42 +0100

    build: lazy theme path
    closes #260

--- packages/screenshot/src/pretty/theme.js
+++ packages/screenshot/src/pretty/theme.js
@@ -4,15 +4,18 @@ const { readFile } = require('fs').promises
 const isUrl = require('is-url-http')
 const path = require('path')
 
-const THEME_CACHE = Object.create(null)
-const THEME_PATHS = path.resolve(require.resolve('prism-themes'), '../themes')
+const CACHE = Object.create(null)
+
+const GET_THEME_PATH = () => path.resolve(require.resolve('prism-themes'), '../themes')
+
+const THEME_PATH = () => CACHE.root || (CACHE.root = GET_THEME_PATH())
 
 module.exports = async themeId => {
   if (isUrl(themeId)) return `<link rel="stylesheet" type="text/css" href="${themeId}">`
 
   const stylesheet =
-    THEME_CACHE[themeId] ||
-    (THEME_CACHE[themeId] = await readFile(path.resolve(THEME_PATHS, `prism-${themeId}.css`)))
+    CACHE[themeId] ||
+    (CACHE[themeId] = await readFile(path.resolve(THEME_PATH(), `prism-${themeId}.css`)))
 
   return `<style>${stylesheet}</style>`
 }
