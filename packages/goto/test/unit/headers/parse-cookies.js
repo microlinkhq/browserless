513a516427e96ab06a63c66b2fd3370edb049c4b
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-05-14 23:06:30 +0200

    refactor: better cookies parser implementation

--- packages/goto/test/unit/headers/parse-cookies.js
+++ packages/goto/test/unit/headers/parse-cookies.js
@@ -10,44 +10,29 @@ test('parse cookies from string with `; ` delimiter', t => {
 
   const cookies = parseCookies(url, cookiesStr)
 
-  t.deepEqual(cookies, [
-    {
-      name: 'foo',
-      value: 'bar',
-      domain: '.example.com',
-      url: 'https://example.com',
-      path: '/'
-    },
-    {
-      name: 'hello',
-      value: 'world',
-      domain: '.example.com',
-      url: 'https://example.com',
-      path: '/'
-    }
-  ])
+  t.true(cookies.every(cookie => !!cookie.value))
+  t.true(cookies.every(cookie => !!cookie.domain))
+  t.true(cookies.every(cookie => !!cookie.path))
+  t.true(cookies.every(cookie => !!cookie.name))
 })
 
 test('parse cookies from string with `;` delimiter', t => {
   const url = 'https://example.com'
-  const cookiesStr = 'foo=bar; hello=world'
+  const cookiesStr = 'foo=bar;hello=world'
 
   const cookies = parseCookies(url, cookiesStr)
 
-  t.deepEqual(cookies, [
-    {
-      name: 'foo',
-      value: 'bar',
-      domain: '.example.com',
-      url: 'https://example.com',
-      path: '/'
-    },
-    {
-      name: 'hello',
-      value: 'world',
-      domain: '.example.com',
-      url: 'https://example.com',
-      path: '/'
-    }
-  ])
+  t.true(cookies.every(cookie => !!cookie.value))
+  t.true(cookies.every(cookie => !!cookie.domain))
+  t.true(cookies.every(cookie => !!cookie.path))
+  t.true(cookies.every(cookie => !!cookie.name))
+})
+
+test('works fine with subdomains', t => {
+  const url = 'https://this.is.an.example.com'
+  const cookiesStr = 'foo=bar;hello=world'
+
+  const cookies = parseCookies(url, cookiesStr)
+
+  t.true(cookies.every(cookie => cookie.domain === 'this.is.an.example.com'))
 })
