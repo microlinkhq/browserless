a9bfa1e72b3b9b8600ad21325cc50408a481d148
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-10-12 11:42:12 +0000

    chore(cli): add theme customization based on system colors

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -4,6 +4,7 @@
 
 const createBrowserless = require('browserless')
 const beautyError = require('beauty-error')
+const darkMode = require('dark-mode')
 const path = require('path')
 const fs = require('fs')
 
@@ -11,10 +12,20 @@ const commands = fs.readdirSync(path.resolve(__dirname, 'commands'))
 
 const cli = require('meow')({
   pkg: require('../package.json'),
-  help: require('./help')(commands)
+  help: require('./help')(commands),
+  flags: {
+    headless: {
+      type: 'boolean',
+      default: process.env.HEADLESS !== undefined ? process.env.HEADLESS : true
+    },
+    codeScheme: {
+      type: 'string',
+      default: 'ghcolors'
+    }
+  }
 })
 
-const { verbose } = cli.flags
+const { verbose, headless } = cli.flags
 
 const spinner = verbose ? require('./spinner') : { start: () => {}, stop: () => {} }
 
@@ -25,8 +36,16 @@ const run = async () => {
   const url = new URL(rawUrl).toString()
   const fn = require(`./commands/${command}`)
 
-  const browserlessFactory = createBrowserless()
+  const browserlessFactory = createBrowserless({ headless })
   const browserless = await browserlessFactory.createContext()
+
+  if (cli.flags.codeScheme === 'ghcolors') {
+    const isDark = await darkMode.isDark()
+    cli.flags.colorScheme = isDark ? 'dark' : 'light'
+    cli.flags.styles = isDark
+      ? '#screenshot pre{background:#000}#screenshot .token.string{color:#50e3c2}#screenshot .token.number{color:#f81ce5}'
+      : '#screenshot pre{background:#fff}#screenshot .token.string{color:#f81ce5}#screenshot .token.number{color:#50e3c2}'
+  }
 
   const result = await fn({ url, browserless, opts: cli.flags })
 
