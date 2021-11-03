e687a45ceba7eac73ddf685c022843b9a2e01597
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-11-03 22:49:12 +0100

    Revert "chore(screenshot): check for idle time"
    This reverts commit bbf02e380c18d2d3b20f71f941b55b01941c32ed.

--- packages/screenshot/src/index.js
+++ packages/screenshot/src/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const { PuppeteerPendingRequests } = require('@agabhane/puppeteer-pending-requests')
 const debug = require('debug-logfmt')('browserless:screenshot')
 const createGoto = require('@browserless/goto')
 const prettyMs = require('pretty-ms')
@@ -94,13 +93,11 @@ module.exports = ({ goto, ...gotoOpts }) => {
       screenshot = await page.screenshot({ ...opts, ...screenshotOpts })
       debug('screenshot', { waitUntil, duration: prettyMs(timeScreenshot()) })
     } else {
-      const puppeteerPendingRequests = new PuppeteerPendingRequests(page)
       ;({ response } = await goto(page, { ...opts, url, waitUntilAuto }))
       async function waitUntilAuto (page, { response }) {
         const [screenshotOpts] = await Promise.all([
           waitForElement(page, element),
-          beforeScreenshot(response),
-          puppeteerPendingRequests.waitForNetworkIdle(100)
+          beforeScreenshot(response)
         ])
         const { isWhite } = await takeScreenshot({ ...opts, ...screenshotOpts })
         debug('screenshot', { waitUntil, isWhite, duration: prettyMs(timeScreenshot()) })
