48ab35b9e8811653b989ebab97d8c40196860d95
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-11-16 23:43:42 +0100

    chore(goto): adjust timeout

--- packages/goto/src/index.js
+++ packages/goto/src/index.js
@@ -277,7 +277,7 @@ module.exports = ({
 
     const { value } = await run({
       fn: html ? page.setContent(html, args) : page.goto(url, args),
-      timeout: timeout * 0.9,
+      timeout: timeout * 0.8,
       debug: html ? 'html' : 'url'
     })
 
