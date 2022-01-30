7b28653e7fa0cc8fc3812d33f7fe73af88bc0aaa
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-01-30 18:33:28 +0100

    fix: ensure destroy context is fullfilled

--- packages/browserless/test/index.js
+++ packages/browserless/test/index.js
@@ -5,35 +5,29 @@ const createBrowserless = require('../src')
 
 require('@browserless/test')(createBrowserless())
 ;['pdf', 'screenshot', 'html', 'text'].forEach(method => {
-  test.skip(`.${method} wrap errors`, async t => {
+  test(`.${method} wrap errors`, async t => {
     const timeout = 50
 
     const browserlessFactory = createBrowserless({ timeout })
     const browserless = await browserlessFactory.createContext()
 
+    t.teardown(() => browserless.destroyContext())
+
     const error = await t.throwsAsync(
       browserless[method]('https://example.com', { adblock: false, animations: true })
     )
 
-    await browserless.destroyContext()
-
+    t.truthy(error)
     t.is(error.name, 'BrowserlessError')
     t.is(error.code, 'EBRWSRTIMEOUT')
     t.is(error.message, `EBRWSRTIMEOUT, Promise timed out after ${timeout} milliseconds`)
-
-    const browser = await browserless.browser()
-    const pages = await browser.pages()
-
-    t.is(pages.length, 1) // about:page is always open
-
-    await browserless.destroyContext()
-    await browserlessFactory.close()
   })
 })
 
 test('ensure to destroy browser contexts', async t => {
   const browserlessFactory = createBrowserless()
   const browser = await browserlessFactory.browser()
+  t.teardown(() => browser.close())
 
   t.is(browser.browserContexts().length, 1)
 
@@ -46,6 +40,4 @@ test('ensure to destroy browser contexts', async t => {
   await browserless.destroyContext()
 
   t.is(browser.browserContexts().length, 1)
-
-  await browser.close()
 })
