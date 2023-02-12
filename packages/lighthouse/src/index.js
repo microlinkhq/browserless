4cfe873f18192daa6b01ca1e85ac1d9b06cf21af
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-02-12 23:45:42 +0100

    refactor(lighthouse): unify flags and config

--- packages/lighthouse/src/index.js
+++ packages/lighthouse/src/index.js
@@ -3,34 +3,13 @@
 const lighthouse = require('./lighthouse')
 const getConfig = require('./get-config')
 
-module.exports = getBrowserless => async (
-  url,
-  {
-    configPath,
-    hostname,
-    logLevel = 'error',
-    output = 'json',
-    plugins,
-    port,
-    timeout,
-    ...opts
-  } = {}
-) => {
+module.exports = getBrowserless => async (url, { output = 'json', timeout, ...opts } = {}) => {
   let teardown
   const browserless = await getBrowserless(fn => (teardown = fn))
 
   const fn = page => async () =>
     lighthouse({
-      config: await getConfig(opts),
-      // https://github.com/GoogleChrome/lighthouse/blob/b0321f418b01b84c837a3af61337e5a811f75552/types/externs.d.ts#L15
-      flags: {
-        configPath,
-        hostname,
-        logLevel,
-        output,
-        plugins,
-        port
-      },
+      config: await getConfig({ ...opts, output }),
       page,
       url
     })
