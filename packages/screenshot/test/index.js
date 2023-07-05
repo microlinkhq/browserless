ca3afea1fd69eb13fd84c1f3331557b2c8952608
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-07-05 10:24:31 +0200

    build: debug

--- packages/screenshot/test/index.js
+++ packages/screenshot/test/index.js
@@ -4,7 +4,7 @@ const { getBrowserContext } = require('@browserless/test/util')
 const cheerio = require('cheerio')
 const test = require('ava')
 
-test('graphics features', async t => {
+test.only('graphics features', async t => {
   const browserless = await getBrowserContext(t)
 
   const getGpu = browserless.withPage(page => async () => {
@@ -18,17 +18,15 @@ test('graphics features', async t => {
     const props = []
 
     $('.feature-status-list li').each(function () {
-      props.push(
-        $(this)
-          .text()
-          .split(': ')
-      )
+      props.push($(this).text().split(': '))
     })
 
     return Object.fromEntries(props)
   })
 
   const gpu = await getGpu()
+
+  console.log('graphics features', gpu)
 
   t.is(gpu.WebGL, 'Software only, hardware acceleration unavailable')
   t.is(gpu.WebGL2, 'Software only, hardware acceleration unavailable')
