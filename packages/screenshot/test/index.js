98757c76be6301b857d4f1841adba187888dcedb
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-07-07 00:26:30 +0200

    test: remove debug

--- packages/screenshot/test/index.js
+++ packages/screenshot/test/index.js
@@ -2,9 +2,10 @@
 
 const { getBrowserContext } = require('@browserless/test/util')
 const cheerio = require('cheerio')
+const isCI = require('is-ci')
 const test = require('ava')
 
-test.only('graphics features', async t => {
+test('graphics features', async t => {
   const browserless = await getBrowserContext(t)
 
   const getGpu = browserless.withPage(page => async () => {
@@ -24,10 +25,40 @@ test.only('graphics features', async t => {
     return Object.fromEntries(props)
   })
 
-  const gpu = await getGpu()
-
-  console.log('graphics features', gpu)
-
-  t.is(gpu.WebGL, 'Software only, hardware acceleration unavailable')
-  t.is(gpu.WebGL2, 'Software only, hardware acceleration unavailable')
+  t.deepEqual(
+    await getGpu(),
+    isCI
+      ? {
+          Canvas: 'Software only, hardware acceleration unavailable',
+          'Canvas out-of-process rasterization': 'Disabled',
+          'Direct Rendering Display Compositor': 'Disabled',
+          Compositing: 'Software only. Hardware acceleration disabled',
+          'Multiple Raster Threads': 'Disabled',
+          OpenGL: 'Disabled',
+          Rasterization: 'Software only. Hardware acceleration disabled',
+          'Raw Draw': 'Disabled',
+          'Video Decode': 'Software only. Hardware acceleration disabled',
+          'Video Encode': 'Software only. Hardware acceleration disabled',
+          Vulkan: 'Disabled',
+          WebGL: 'Software only, hardware acceleration unavailable',
+          WebGL2: 'Software only, hardware acceleration unavailable',
+          WebGPU: 'Disabled'
+        }
+      : {
+          Canvas: 'Hardware accelerated',
+          'Canvas out-of-process rasterization': 'Enabled',
+          'Direct Rendering Display Compositor': 'Disabled',
+          Compositing: 'Hardware accelerated',
+          'Multiple Raster Threads': 'Enabled',
+          OpenGL: 'Enabled',
+          Rasterization: 'Hardware accelerated',
+          'Raw Draw': 'Disabled',
+          'Video Decode': 'Hardware accelerated',
+          'Video Encode': 'Hardware accelerated',
+          Vulkan: 'Disabled',
+          WebGL: 'Hardware accelerated',
+          WebGL2: 'Hardware accelerated',
+          WebGPU: 'Hardware accelerated'
+        }
+  )
 })
