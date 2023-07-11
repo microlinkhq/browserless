e64a597984e2e6b2d5b4bacae7de204637d89237
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-07-11 12:37:05 +0200

    test: update assertions

--- packages/browserless/test/driver.js
+++ packages/browserless/test/driver.js
@@ -7,7 +7,7 @@ const test = require('ava')
 
 const getChromiumPs = async () => {
   const ps = await psList()
-  return ps.filter(ps => ps.name.includes('Chromium'))
+  return ps.filter(ps => ps.name === 'Google Chrome for Testing')
 }
 
 ;(isCI ? test.skip : test.serial)('.close() will kill process and subprocess', async t => {
@@ -20,15 +20,14 @@ const getChromiumPs = async () => {
   t.is((await getChromiumPs())[0].pid, (await browserlessFactory.browser()).process().pid)
 
   const browserless = await browserlessFactory.createContext()
-  t.is((await getChromiumPs()).length, 3)
+  t.is((await getChromiumPs()).length, 1)
 
   await browserless.destroyContext()
-  t.is((await getChromiumPs()).length, 3)
+  t.is((await getChromiumPs()).length, 1)
 
   await browserlessFactory.close()
-  t.is((await getChromiumPs()).length, initialPs)
+  t.is((await getChromiumPs()).length, initialPs.length)
 })
-
 ;(isCI ? test.skip : test.serial)('.close() is idempotency', async t => {
   const initialPs = await getChromiumPs()
 
@@ -36,8 +35,8 @@ const getChromiumPs = async () => {
   t.is((await getChromiumPs()).length, 1)
 
   await browserlessFactory.close()
-  t.is((await getChromiumPs()).length, initialPs)
+  t.is((await getChromiumPs()).length, initialPs.length)
 
   await browserlessFactory.close()
-  t.is((await getChromiumPs()).length, initialPs)
+  t.is((await getChromiumPs()).length, initialPs.length)
 })
