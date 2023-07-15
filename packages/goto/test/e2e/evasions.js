70facebacab6dd009bc01213b7555d0a47f4b953
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-07-15 12:56:29 +0200

    test(goto): add creepjs evasions test

--- packages/goto/test/e2e/evasions.js
+++ packages/goto/test/e2e/evasions.js
@@ -18,6 +18,29 @@ test('arh.antoinevastel.com/bots/areyouheadless', async t => {
   t.true(assertion)
 })
 
+test('creepjs', async t => {
+  const browserless = await getBrowserContext(t)
+
+  const fingerprint = await browserless.evaluate(page =>
+    page.evaluate(async () => {
+      const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+      let result = 'Computing...'
+      do {
+        await delay(100)
+        result = document.querySelector('.fingerprint-header div').innerText.split(': ')[1]
+      } while (result === 'Computing...')
+      return result
+    })
+  )
+
+  const [fingerprintOne, fingerprintTwo] = await Promise.all([
+    fingerprint('https://abrahamjuliot.github.io/creepjs/', { timezone: 'Europe/Madrid' }),
+    fingerprint('https://abrahamjuliot.github.io/creepjs/', { timezone: 'Europe/Paris' })
+  ])
+
+  t.true(fingerprintOne !== fingerprintTwo)
+})
+
 test('fingerprintjs', async t => {
   const browserless = await getBrowserContext(t)
 
