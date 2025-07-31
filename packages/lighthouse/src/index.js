bf6177073a862fa39d1cbbbda9e04642865534ed
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-07-31 23:24:44 +0200

    fix(lighthouse): pass flags

--- packages/lighthouse/src/index.js
+++ packages/lighthouse/src/index.js
@@ -5,12 +5,13 @@ const getConfig = require('./get-config')
 
 module.exports =
   getBrowserless =>
-    async (url, { output = 'json', timeout, ...opts } = {}) => {
+    async (url, { output = 'json', timeout, flags, ...opts } = {}) => {
       let teardown
       const browserless = await getBrowserless(fn => (teardown = fn))
 
       const fn = page => async () =>
         lighthouse({
+          flags,
           config: await getConfig({ ...opts, output }),
           page,
           url
--- packages/lighthouse/src/lighthouse.js
+++ packages/lighthouse/src/lighthouse.js
@@ -2,7 +2,7 @@
 
 const lighthouse = require('lighthouse/core/index.cjs')
 
-module.exports = async ({ url, config, page }) => {
-  const { lhr, report } = await lighthouse(url, undefined, config, page)
+module.exports = async ({ url, config, flags, page }) => {
+  const { lhr, report } = await lighthouse(url, flags, config, page)
   return config.settings.output === 'json' ? lhr : report
 }
