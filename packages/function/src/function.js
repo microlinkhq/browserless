956514f81a40377bb4d0250f440d1fd9099b221f
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-12-02 10:42:17 +0100

    fix(function): enable network permission for page interaction

--- packages/function/src/function.js
+++ packages/function/src/function.js
@@ -3,8 +3,12 @@
 const isolatedFunction = require('isolated-function')
 const template = require('./template')
 
+const [nodeMajor] = process.version.slice(1).split('.').map(Number)
+
 module.exports = async ({ url, code, vmOpts, browserWSEndpoint, ...opts }) => {
-  const [fn, teardown] = isolatedFunction(template(code), { ...vmOpts, throwError: false })
+  const needsNetwork = template.isUsingPage(code)
+  const allow = needsNetwork && nodeMajor >= 25 ? ['net'] : []
+  const [fn, teardown] = isolatedFunction(template(code), { ...vmOpts, allow, throwError: false })
   const result = await fn(url, browserWSEndpoint, opts)
   await teardown()
   return result
--- packages/function/src/template.js
+++ packages/function/src/template.js
@@ -3,30 +3,34 @@
 const walk = require('acorn-walk')
 const acorn = require('acorn')
 
-module.exports = code => {
+const isUsingPage = code => {
   const ast = acorn.parse(code, { ecmaVersion: 2023, sourceType: 'module' })
 
-  let isUsingPage = false
+  let result = false
 
   walk.simple(ast, {
     ObjectPattern (node) {
       node.properties.forEach(prop => {
         if (prop.type === 'Property' && prop.key.name === 'page') {
-          isUsingPage = true
+          result = true
         }
         if (prop.type === 'RestElement' && prop.argument.name === 'page') {
-          isUsingPage = true
+          result = true
         }
       })
     },
     MemberExpression (node) {
       if (node.property.name === 'page' || node.property.value === 'page') {
-        isUsingPage = true
+        result = true
       }
     }
   })
 
-  if (!isUsingPage) return `async (url, _, opts) => (${code})(opts)`
+  return result
+}
+
+const template = code => {
+  if (!isUsingPage(code)) return `async (url, _, opts) => (${code})(opts)`
   return `
     async (url, browserWSEndpoint, opts) => {
       const puppeteer = require('@cloudflare/puppeteer')
@@ -40,3 +44,6 @@ module.exports = code => {
       }
     }`
 }
+
+module.exports = template
+module.exports.isUsingPage = isUsingPage
