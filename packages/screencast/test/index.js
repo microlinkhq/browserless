f0313c81ff647cbd9618136b343294bd8a724ff2
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-02-27 13:53:46 +0100

    perf(screencast): clean cdp frame listeners on stop

--- packages/screencast/test/index.js
+++ packages/screencast/test/index.js
@@ -31,3 +31,35 @@ test('capture frames', async t => {
     t.truthy(metadata.timestamp)
   })
 })
+
+test('clean up cdp frame listeners across screencast sessions', async t => {
+  const browserless = await getBrowserContext(t)
+  const page = await browserless.page()
+  const cdp = page._client()
+
+  const countListeners = () => cdp.listenerCount('Page.screencastFrame')
+
+  const screencastA = createScreencast(page, {
+    quality: 0,
+    format: 'png',
+    everyNthFrame: 1
+  })
+
+  t.is(countListeners(), 0)
+
+  await screencastA.start()
+  t.is(countListeners(), 1)
+  await screencastA.stop()
+  t.is(countListeners(), 0)
+
+  const screencastB = createScreencast(page, {
+    quality: 0,
+    format: 'png',
+    everyNthFrame: 1
+  })
+
+  await screencastB.start()
+  t.is(countListeners(), 1)
+  await screencastB.stop()
+  t.is(countListeners(), 0)
+})
