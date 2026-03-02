ee4f763f34c3e968ff88fee1ed49ca0f0b926cb0
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-02 22:26:55 +0100

    ci: use node24

--- packages/capture/src/constants.js
+++ packages/capture/src/constants.js
@@ -3,7 +3,7 @@
 const path = require('path')
 
 module.exports = {
-  DEFAULT: { duration: 3000, type: 'webm' },
+  DEFAULT: Object.freeze({ duration: 3000, type: 'webm' }),
   EXTENSION_ID: 'jjndjgheafjngoipoacpjgeicjeomjli',
   EXTENSION_PATH: path.join(__dirname, '..', 'extension'),
   TYPES: Object.freeze(['webm', 'mp4']),
