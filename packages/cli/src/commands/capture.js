6b52774aafe4819be8d5756fbb7c08b622c558dd
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-02 12:12:27 +0100

    chore: useDefaultContext

--- packages/cli/src/commands/capture.js
+++ packages/cli/src/commands/capture.js
@@ -1,29 +1,20 @@
 'use strict'
 
 const prettyBytes = require('pretty-bytes')
-const createCapture = require('@browserless/capture')
 
 module.exports = async ({ url, browserless, opts }) => {
-  const browser = await browserless.browser()
-  const page = await browser.newPage()
-  const capture = createCapture({ goto: browserless.goto })
-
   const { path: outputPath, ...captureOpts } = opts
+
   const type = captureOpts.type && String(captureOpts.type).toLowerCase()
+
   const suggestedType =
     { matroska: 'mkv', mkv: 'mkv', mp4: 'mp4', webm: 'webm' }[type] ||
     (captureOpts.mimeType && captureOpts.mimeType.includes('mp4') ? 'mp4' : 'webm')
 
-  let video
-
-  try {
-    video = await capture(page)(url, {
-      ...captureOpts,
-      path: outputPath
-    })
-  } finally {
-    await page.close().catch(() => {})
-  }
+  const video = await browserless.capture(url, {
+    ...captureOpts,
+    path: outputPath
+  })
 
   const preview = outputPath
     ? `saved at ${process.cwd()}/${outputPath}`
