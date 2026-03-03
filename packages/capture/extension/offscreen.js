18f4998f9c0b66ecd5a0c52a83ffbd51922b5115
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-03-03 11:17:22 +0100

    chore: tweaks

--- packages/capture/extension/offscreen.js
+++ packages/capture/extension/offscreen.js
@@ -125,7 +125,7 @@ const START_RECORDING = async ({
     throw error
   }
 
-  const recorder = new MediaRecorder(stream, { mimeType, ...(recorderOptions || {}) })
+  const recorder = new MediaRecorder(stream, { mimeType, ...recorderOptions })
 
   const pending = new Set()
 
